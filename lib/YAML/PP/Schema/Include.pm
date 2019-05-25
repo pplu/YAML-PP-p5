1b9390ef23c8ed314bacaae842e9e016f00c310e
Tina Müller <cpan2@tinita.de>
2019-05-26 01:54:38 +0200

    WIP

--- lib/YAML/PP/Schema/Include.pm
+++ lib/YAML/PP/Schema/Include.pm
@@ -73,18 +73,51 @@ YAML::PP::Schema::Include - Include YAML files
 =head1 SYNOPSIS
 
     my $include_paths = ["/path/to/include/yaml/1", "/path/to/include/yaml/2"];
+    my %pp_args = (
+        boolean => 'JSON::PP',
+        cyclic_refs => 'fatal',
+    );
     my $include = YAML::PP::Schema::Include->new(
         paths => $include_paths,
-        pp_args => {
-            boolean => 'JSON::PP',
-            cyclic_refs => 'fatal',
-        },
+        pp_args => \%pp_args,
     );
-    my $yp = YAML::PP->new( schema => ['JSON', $include] );
+    my $yp = YAML::PP->new( schema => ['JSON', $include], %pp_args );
+
+    # file1.yaml
+    # ---
+    # a: b
 
     my $yaml = <<'EOM';
-    - !include file1.yaml
+    - included: !include file1.yaml
     EOM
     my ($data) = $yp->load_string($yaml);
+
+    # The result will be:
+    $data = {
+        included => { a => 'b' }
+    };
+
+=head1 DESCRIPTION
+
+This plugin allows you to split a large YAML file into smaller ones.
+
+You must specify the paths where to search for files to include. It iterates
+through the paths and returns the first filename that exists.
+
+If the included file contains more than one document, only the first one
+will be included.
+
+I will probably add a possibility to return all documents as an arrayref.
+
+The included YAML file will be loaded by creating a new L<YAML::PP> object
+with the schema from the existing object. This way you can recursively include
+files.
+
+You can even reuse the same include via an alias:
+
+    ---
+    invoice:
+        shipping address: &address !include address.yaml
+        billing address: *address
 
 =cut
